3045640a475669ed00acf4fe024cc99a1f904bd6
venkat <venkatraj.g505@gmail.com>
2025-01-26 13:43:18 +0530

    added checkHuman method in justbesure.js file

--- Justbesure.js
+++ Justbesure.js
@@ -1,9 +1,10 @@
 class Main {
-    constructor(name,id,salary,gender){
+    constructor(name,id,salary,gender,Human){
         this.name = name,
         this.id = parseInt(id),
         this.salary = parseFloat(salary),
-        this.gender = gender
+        this.gender = gender,
+        this.Human = Human
     }
 
     nameAndId(){
@@ -13,9 +14,13 @@ class Main {
     salaryAndGender(){
         console.log(this.salary,this.gender)
     }
+    checkHuman(){
+        console.log(this.Human)
+    }
 }
 
-const detials = new Main(`Gopal`,'145','2939.40','Male')
+const detials = new Main(`Gopal`,'145','2939.40','Male',`He is Human`)
 
 detials.nameAndId()
 detials.salaryAndGender()
+detials.checkHuman()
